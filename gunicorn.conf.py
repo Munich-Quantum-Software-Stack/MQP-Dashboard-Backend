e7ea5c7aa05df73781e684723176e8e40bc9f6fa
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2023-12-13 15:50:21 +0100

    Added key paths

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,5 @@
 bind = "0.0.0.0:5000"
 workers = 1
 wsgi_app = "bqp_dashboard_backend:create_app()"
+certfile = '/etc/bqp-certs/portal_quantum_lrz_de.crt.pem'
+keyfile = '/etc/bqp-certs/portal-sec-key-ohnepass.pem'
