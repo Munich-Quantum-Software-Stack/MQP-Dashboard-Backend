179270fa4d605224301b78d595325763f83287d0
Martin Ruefenacht <martin.ruefenacht@lrz.de>
2023-12-18 10:54:47 +0100

    Added custom handler for jwt not present

--- bqp_dashboard_backend/__init__.py
+++ bqp_dashboard_backend/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask import Response
 from pony.flask import Pony
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from eliot import add_destinations
+from eliot import add_destinations, log_message
 from eliot.journald import JournaldDestination
+import http
 
 from . import login
 from . import tokens
@@ -14,12 +16,19 @@ from . import resources
 add_destinations(JournaldDestination())
 
 
+def on_no_jwt_provided(message: str):
+    log_message("attempt without JWT - " + message)
+
+    return Response(status=http.HTTPStatus.UNAUTHORIZED)
+
+
 def create_app():
     app = Flask(__name__)
     app.config["JWT_SECRET_KEY"] = "7nFVM3TwqmCZeC7goIwM1DtEAQKfAmWF"
 
     CORS(app)
-    JWTManager(app)
+    jwt = JWTManager(app)
+    jwt.unauthorized_loader()
     Pony(app)
 
     app.register_blueprint(login.BLUEPRINT)
