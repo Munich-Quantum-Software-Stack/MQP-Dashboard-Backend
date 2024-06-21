8e3b569d97bc1dcbbd1265dcc5395aa12645abf8
Nitish Kumar <Nitish.Kumar@lrz.de>
2024-06-21 16:21:42 +0200

    added test pem file

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 bind = "0.0.0.0:5000"
 workers = 1
 wsgi_app = "bqp_dashboard_backend:create_app()"
-certfile = '/etc/bqp-certs/portal_quantum_lrz_de.crt.pem'
-keyfile = '/etc/bqp-certs/portal-sec-key-ohnepass.pem'
+certfile = '/etc/bqp-certs/portal-test_quantum_lrz_de.crt.pem'
+keyfile = '/etc/bqp-certs/portal-test-sec-key-ohnepass.pem'
