4f91c9284877d0a4562f04d71fae1c9dc25eaf79
Bauer, Tobias <tobias.bauer@lrz.de>
2024-08-28 16:10:51 +0200

    Merge-request for Trang

--- bqp_dashboard_backend/resources.py
+++ bqp_dashboard_backend/resources.py
@@ -17,9 +17,21 @@ def fetch_all_resources():
     """Fetch all jobs belonging to a user."""
 
     identity = get_jwt_identity()
-    #resources = database.resources.fetch_resources_available_to_identity(identity)
-    resources = database.resources.fetch_all_resources()
-    sanitized_resources = [resource.to_dict() for resource in resources]
-    sorted_resources_by_name = sorted(sanitized_resources, key=lambda x: x["name"], reverse=False)
+    try:
+        available_resources = database.resources.fetch_resources_available_to_identity(identity)
+        sanitized_available_resources = [resource.to_dict() for resource in available_resources]
+        resources = database.resources.fetch_all_resources()
+        sanitized_resources = [resource.to_dict() for resource in resources]
+        #restricted_resources = database.fetch_resources_restricted_to_identity(identity)
+        #sanitized_restricted_resources = [resource.to_dict() for resource in restricted_resources]
+        sorted_resources_by_name = sorted(sanitized_resources, key=lambda x: x["name"], reverse=False)
 
-    return {"resources": sorted_resources_by_name}, HTTPStatus.OK
+        return { 
+            "resources": sorted_resources_by_name, 
+            "available_resources": sanitized_available_resources ,
+            #"restricted_resources": sanitized_restricted_resources
+        }, HTTPStatus.OK
+    except TypeError as error:
+        return {
+            "error_message": error
+        }, HTTPStatus.FORBIDDEN
