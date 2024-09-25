5d676817e63312c3cbd4055e11074230f9d21187
Huynh, Trang <trang.huynh@lrz.de>
2024-09-25 15:23:59 +0200

    Merging updates from branch "backend_trang"

--- bqp_dashboard_backend/resources.py
+++ bqp_dashboard_backend/resources.py
@@ -22,14 +22,13 @@ def fetch_all_resources():
         sanitized_available_resources = [resource.to_dict() for resource in available_resources]
         resources = database.resources.fetch_all_resources()
         sanitized_resources = [resource.to_dict() for resource in resources]
-        #restricted_resources = database.fetch_resources_restricted_to_identity(identity)
-        #sanitized_restricted_resources = [resource.to_dict() for resource in restricted_resources]
+
         sorted_resources_by_name = sorted(sanitized_resources, key=lambda x: x["name"], reverse=False)
+        sorted_available_resources = sorted(sanitized_available_resources, key=lambda y: y["name"], reverse=False) 
 
         return { 
             "resources": sorted_resources_by_name, 
-            "available_resources": sanitized_available_resources ,
-            #"restricted_resources": sanitized_restricted_resources
+            "available_resources": sorted_available_resources,
         }, HTTPStatus.OK
     except TypeError as error:
         return {
