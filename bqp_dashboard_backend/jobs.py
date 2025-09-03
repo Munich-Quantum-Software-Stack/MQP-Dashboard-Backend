d5de7da4a2eda7849905ec1c640d7e33f6d38b89
Tobias Bauer <di35hej@badwlrz-cl14639>
2025-09-03 11:11:45 +0200

    added filter, order, and order_by keyword

--- bqp_dashboard_backend/jobs.py
+++ bqp_dashboard_backend/jobs.py
@@ -11,19 +11,42 @@ BLUEPRINT = Blueprint("jobs", __name__)
 @jwt_required()
 @log_call
 def fetch_all_jobs():
-    """Fetch all jobs belonging to a user and return range [jpp*p:(jpp+1)*p] ob jobs and number of total jobs."""
+    """
+    Return a paginated list of jobs for the current user.
+
+    Args:
+        page (int): Page number of results.
+        jobs_per_page (int): Number of jobs per page.
+        order (str): Sort order, "ASC" or "DESC".
+        order_by (str): Field to order by. "ID", "STATUS" or "DATE", where the latter is timestamp_submitted. 
+
+    Returns:
+        dict: Jobs in the requested range and total number of jobs.
+    """
     page_nr=request.args.get('p')
-    jpp=request.args.get('jpp')
     if not page_nr:
         page_nr=0
     else:
         page_nr=int(page_nr)
+
+    jpp=request.args.get('jpp')
     if not jpp:
         jpp=20
     else:
         jpp=int(jpp)
+
+    order=request.args.get('order')
+    if not order:
+        order="ASC"
+
+    order_by=request.args.get('order_by')
+    if not order_by:
+        order_by = "ID"
+
+    filter=request.args.get('filter')
+
     identity = get_jwt_identity()
-    jobs = database.jobs.fetch_by_identity_pages(identity=identity, page=page_nr, jobs_per_page=jpp)
+    jobs = database.jobs.fetch_by_identity_pages(identity=identity, page=page_nr, jobs_per_page=jpp, order=order, order_by=order_by, filter=filter)
     totaljob_nr = database.jobs.fetch_by_identity_total_job_nr(identity)
     sanitized_jobs = [job.to_dict() for job in jobs]
     sorted_jobs_by_id = sorted(sanitized_jobs, key=lambda x: x["id"], reverse=True)
