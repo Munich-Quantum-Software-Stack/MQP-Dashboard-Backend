6feb2a2f3df6e5dc7dc4e7af0db03b6674d461d1
Tobias Bauer <di35hej@badwlrz-cl14639>
2025-09-03 12:03:04 +0200

    fixed jobs-retrieval

--- bqp_dashboard_backend/jobs.py
+++ bqp_dashboard_backend/jobs.py
@@ -44,11 +44,11 @@ def fetch_all_jobs():
     if not order_by:
         order_by = "ID"
 
-    filter=request.args.get('filter')
+    filter_query=request.args.get('filter')
 
     identity = get_jwt_identity()
-    jobs = database.jobs.fetch_by_identity_pages(identity=identity, page=page_nr, jobs_per_page=jpp, order=order, order_by=order_by, filter=filter)
-    totaljob_nr = database.jobs.fetch_by_identity_total_job_nr(identity)
+    query = database.jobs.fetch_by_identity_pages(identity=identity, page=page_nr, jobs_per_page=jpp, order=order, order_by=order_by, filter=filter_query)
+    jobs, totaljob_nr = query["jobs"], query["totaljob_nr"]
     sanitized_jobs = [job.to_dict() for job in jobs]
     sorted_jobs_by_id = sorted(sanitized_jobs, key=lambda x: x["id"], reverse=True)
     return {"jobs": sorted_jobs_by_id, "totaljob_nr": totaljob_nr}, HTTPStatus.OK
