95ab453b6b97946bd6ee4742b44595f516ca4396
Ege Özmeral <ege.oezmeral@gmail.com>
2026-03-24 11:16:31 +0100

    New Approach to ci

--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,8 @@
+import nox
+from nox.sessions import Session
+
+
 @nox.session(python=False)
 def ci_tests(session: Session) -> None:
     """Run CI-safe syntax tests."""
-    session.run("uv", "run", "pytest", "-v", "-s", "ci_tests/", external=True)
+    session.run("python", "-m", "pytest", "-v", "-s", "ci_tests/", external=True)
