a8f9a51fe4fc0bb186cd3d35ef3a35bab46b8d00
Ege Özmeral <ege.oezmeral@gmail.com>
2026-05-26 11:46:52 +0200

    Modified conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,11 +2,11 @@ import json
 import pytest
 import os
 from pathlib import Path
-from mqp_dashboard_backend import create_app
+
+# from mqp_dashboard_backend import create_app
 from bqp_database_access._database import open_database
 import bqp_database_access as database_access
 from pony.orm import db_session
-from pony.orm import TransactionError
 from http import HTTPStatus
 from werkzeug.datastructures import Headers
 from ldap_test import LdapServer
@@ -19,6 +19,9 @@ with TEST_SEED_CONFIG_PATH.open(encoding="utf-8") as config_file:
 
 @pytest.fixture(scope="module")
 def app():
+    create_local_database()
+    from mqp_dashboard_backend import create_app
+
     app = create_app()
     app.config.update(
         {
@@ -26,8 +29,6 @@ def app():
         }
     )
 
-    # other setup can go here
-    create_local_database()
     server = create_ldap_server()
 
     yield app
@@ -84,190 +85,56 @@ def create_ldap_server():
 def create_local_database():
     delete_local_database()
 
-    db = open_database(create_tables=True)
-    db.disconnect()
+    quantum_db = open_database(create_tables=True)
 
-    try:
-        with db_session:
-            database_access.users.create_new_security_level(
-                "BASIC",
-                token_max_live_count=1,
-                token_max_lifetime=30,
-                token_min_creation_interval=1,
-                token_max_jobs=100,
-                token_max_budget=100,
-                token_max_rate=1,
-                login_max_interval=365,
-            )
+    with db_session:
+        database_access.users.create_new_security_level(
+            "BASIC",
+            token_max_live_count=1,
+            token_max_lifetime=30,
+            token_min_creation_interval=1,
+            token_max_jobs=100,
+            token_max_budget=100,
+            token_max_rate=1,
+            login_max_interval=365,
+        )
 
-            database_access.users.create_new_user_with_secret(
-                "test_user",
-                "test_password",
-                "BASIC",
-                "test@test.mail",
-                "TEST_HPC_CENTER",
-                "QUANTUM",
-            )
-            """
-            database_access.users.create_new_user_with_secret(
-                "test_user",
-                "test_password",
-                "BASIC",
-                "test@test.mail",
-                "TEST_HPC_CENTER",
-                "QUANTUM",
-            )
-            """
-            ### This code is to make the test_fetch_all_jobs pass by inserting dummies into the db
-            quantum_db = open_database()
-            quantum_db.insert(
-                "budget", name="temp_budget", note=" ", owner="test_user", credits=1000
-            )
+        database_access.users.create_new_user_with_secret(
+            "test_user",
+            "test_password",
+            "BASIC",
+            "test@test.mail",
+            "TEST_HPC_CENTER",
+            "QUANTUM",
+        )
 
-            quantum_db.insert(
-                "resource_security_level",
-                name="BASIC",
-                note=" ",
-                job_min_interval=10,
-                budget_max_per_job=10,
-                unique_token_required=False,
-                token_max_lifetime=10,
-            )
+        quantum_db.insert("users_in_user_groups", user="test_user", usergroup="MQP_EDU")
 
-            quantum_db.insert(
-                "resource",
-                name="Q5",
-                note=" ",
-                maintenance=False,
-                qubits=5,
-                connectivity=" ",
-                instructions=" ",
-                quantum_technology=" ",
-                num_queued_jobs=1,
-                resource_cost_modifier=1,
-                security_level="BASIC",
-            )
+        quantum_db.insert("user", **TEST_SEED_CONFIG["blocked_user"])
 
-            quantum_db.insert(
-                "target_specification",
-                name="Q5",
-                note=" ",
-                specification_type=" ",
-                minimum_qubits="1",
-                quantum_technology=" ",
-                resource_name="Q5",
-            )
+        database_access.users.create_new_user_with_secret(
+            "ldap_test_user",
+            "test_password",
+            "BASIC",
+            "test@test.mail",
+            "TEST_HPC_CENTER",
+            "LDAP",
+        )
 
-            quantum_db.insert(
-                "circuit_job",
-                id=111,
-                note=" ",
-                status="PENDING",
-                shots=200,
-                circuit="OPENQASM 2.0;",
-                circuit_format="qasm",
-                no_modify=True,
-                timestamp_submitted="2024-07-17 11:17:32.397618",
-                timestamp_scheduled=" 2024-07-17 11:17:32.397618",
-                timestamp_completed=" 2024-07-17 11:17:32.397618",
-                cost=0,
-                result='{"10": 76, "11": 425, "01": 91, "00": 408}',
-                owner="test_user",
-                budget="temp_budget",
-                executed_resource="Q5",
-                target_specification="Q5",
-                executed_circuit="OPENQASM 2.0;",
-                queued=True,
-            )
-            quantum_db.insert(
-                "circuit_job",
-                id=112,
-                note=" ",
-                status="CANCELLED",
-                shots=200,
-                circuit="OPENQASM 2.0;",
-                circuit_format="qasm",
-                no_modify=True,
-                timestamp_submitted="2024-07-17 11:17:32.397618",
-                timestamp_scheduled=" 2024-07-17 11:17:32.397618",
-                timestamp_completed=" 2024-07-17 11:17:32.397618",
-                cost=0,
-                result='{"10": 76, "11": 425, "01": 91, "00": 408}',
-                owner="test_user",
-                budget="temp_budget",
-                executed_resource="Q5",
-                target_specification="Q5",
-                executed_circuit="OPENQASM 2.0;",
-                queued=True,
-            )
-            quantum_db.insert(
-                "circuit_job",
-                id=113,
-                note=" ",
-                status="COMPLETED",
-                shots=200,
-                circuit="OPENQASM 2.0;",
-                circuit_format="qasm",
-                no_modify=True,
-                timestamp_submitted="2024-07-17 11:17:32.397618",
-                timestamp_scheduled=" 2024-07-17 11:17:32.397618",
-                timestamp_completed=" 2024-07-17 11:17:32.397618",
-                cost=0,
-                result='{"10": 76, "11": 425, "01": 91, "00": 408}',
-                owner="test_user",
-                budget="temp_budget",
-                executed_resource="Q5",
-                target_specification="Q5",
-                executed_circuit="OPENQASM 2.0;",
-                queued=True,
-            )
-
-            quantum_db.UserGroup(
-                name="MQP_EDU",
-                note="MQP_EDU group for testing purposes",
-                owner="test_user",
-                cost_modifier=1,
-            )
-
-            quantum_db.insert(
-                "users_in_user_groups", user="test_user", usergroup="MQP_EDU"
-            )
-
-            quantum_db.insert("user", **TEST_SEED_CONFIG["blocked_user"])
-
-            database_access.users.create_new_user_with_secret(
-                "ldap_test_user",
-                "test_password",
-                "BASIC",
-                "test@test.mail",
-                "TEST_HPC_CENTER",
-                "LDAP",
-            )
-
-            database_access.users.create_new_ldap_user(
-                "ldap_test_user",
-                "BASIC",
-                "ldaptest@test.mail",
-                "TEST_HPC_CENTER",
-                "LDAP",
-            )
-    except TransactionError as error:
-        raise error
-    """
-    except TransactionError:
-        pass
-    """
-
-    # temporarily changed for debugging
+        database_access.users.create_new_ldap_user(
+            "ldap_test_user",
+            "BASIC",
+            "ldaptest@test.mail",
+            "TEST_HPC_CENTER",
+            "LDAP",
+        )
 
 
 def delete_local_database() -> None:
-    db = open_database()
-    path = db.provider.pool.filename
-    db.disconnect()
+    sqlite_path = Path(os.getenv("QUANTUM_DB_FILENAME", "test_db.sqlite"))
 
-    if os.path.exists(path):
-        os.remove(path)
+    if sqlite_path.exists():
+        sqlite_path.unlink()
 
 
 @pytest.fixture()
