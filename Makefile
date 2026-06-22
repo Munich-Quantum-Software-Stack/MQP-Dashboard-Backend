27e29d9dfbb8b40ead708e9235bd05c95d786626
Trang Huynh <Trang.Huynh@lrz.de>
2026-06-22 11:47:27 +0200

    Filter credentials files

---
+++ Makefile
@@ -0,0 +1,44 @@
+# Load .env if present
+ifneq (,$(wildcard .env))
+	include .env
+	export
+endif
+
+# Extract version in pyproject.toml
+$(eval VERSION=$(shell grep -m 1 version pyproject.toml | tr -d '"' | cut -d' ' -f3 | tr -s ' ' | tr -d "'" ) )
+
+DOCKER := /usr/local/bin/docker
+IMAGE_NAME = mqp-dashboard-backend
+
+.PHONY: build up down logs run-image tag-image push-image clean
+
+build:
+	$(DOCKER) compose -f docker-compose.yaml build --no-cache
+
+up:
+	$(DOCKER) compose up -d
+
+down:
+	docker compose down
+
+logs:
+	docker compose logs -f
+
+run-image:
+	$(DOCKER) run --rm -it --detach \
+	-p 5000:5000 \
+	--env-file .env \
+	$(IMAGE_NAME):$(VERSION)
+
+tag-image:
+	$(DOCKER) tag $(IMAGE_NAME):$(VERSION) $(IMAGE_NAME):latest
+
+push-image:
+	docker tag $(IMAGE_NAME):${VERSION} ${DOCKERHUB}/$(IMAGE_NAME):${VERSION}
+	docker tag ${DOCKERHUB}/$(IMAGE_NAME):${VERSION} ${DOCKERHUB}/$(IMAGE_NAME):latest
+	docker push ${DOCKERHUB}/$(IMAGE_NAME):${VERSION}
+	docker push ${DOCKERHUB}/$(IMAGE_NAME):latest
+
+clean:
+	$(DOCKER) rmi $(IMAGE_NAME):$(VERSION) || true
+	$(DOCKER) rmi $(IMAGE_NAME):latest || true
---
+++ mqp_dashboard_backend/request_access.py
@@ -0,0 +1,89 @@
+# ------------------------------------------------------------------------------
+# Copyright 2026 Munich Quantum Software Stack Project
+#
+# Licensed under the Apache License, Version 2.0 with LLVM Exceptions (the
+# "License"); you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://github.com/Munich-Quantum-Software-Stack/QDMI/blob/develop/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+#
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# ------------------------------------------------------------------------------
+
+"""MQP Dashboard Request Access Module"""
+
+import os
+from flask import Blueprint, request
+from flask_mail import Message
+from http import HTTPStatus
+from . import config
+
+
+BLUEPRINT = Blueprint("request_access", __name__)
+mail = config.mail
+
+
+@BLUEPRINT.post("/request_access")
+def request_access():
+    """
+    - Receive a request from user and notify to admin about it
+    """
+    if request.method == "POST":
+        request_data = request.get_json()
+        sender = os.getenv("MQP_MAIL_DEFAULT_SENDER")
+        message = Message(
+            subject="New Request Access", sender=("MQP-Dashboard", sender)
+        )
+        message.recipients = [os.getenv("MQP_MAIL_ADMIN")]
+        # message.add_recipient("")
+        message.html = "<p>Hello Admin,<br/>a new request access has been submitted. Please see the content below.<br/><br/>"
+        message.html += "<table><tbody>"
+        message.html += (
+            "<tr><th align='left'>Name: </th><td>"
+            + request_data["title"]
+            + "&nbsp;"
+            + request_data["name"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>Email Address: </th><td>"
+            + request_data["email"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>User-ID: </th><td>"
+            + request_data["userID"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>Project Name: </th><td>"
+            + request_data["project"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>Country: </th><td>"
+            + request_data["country"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>Organization/Institute: </th><td>"
+            + request_data["organization"]
+            + "</td></tr>"
+        )
+        message.html += (
+            "<tr><th align='left'>Message: </th><td>"
+            + request_data["message"]
+            + "</td></tr>"
+        )
+        message.html += "</tbody></table><br/><br/>"
+
+        message.html += "<small>This email was sent automatically. Please do not reply to it.</small>"
+
+        mail.send(message)
+        return {"message": "Mail has sent"}, HTTPStatus.OK
